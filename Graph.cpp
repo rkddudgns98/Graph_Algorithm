05de882d6af2b658c32f4e03fa9982e2c8320e57
rkddudgns98 <42103523+rkddudgns98@users.noreply.github.com>
2020-04-22 23:23:02 +0900

    Graph_Algorithm: Add code that PFS Algorithm

--- Graph.cpp
+++ Graph.cpp
@@ -3,6 +3,7 @@
 #pragma warning(disable: 4996)
 
 #define MAX_NODE 100
+#define UNSEEN (-INT_MAX)
 
 int name2int(char c)
 {
@@ -13,6 +14,8 @@ char int2name(int i)
 	return i + 'A';
 }
 
+//file
+FILE* fp;
 
 //DFS
 int check[MAX_NODE] = { 0, };	//방문했는지 확인
@@ -24,8 +27,14 @@ int APcheck[MAX_NODE] = { 0, };	//방문순서, Spannig tree
 int APorder = 0;	//순서
 int son_of_root = 0;	//자식노드의 개수
 
+//heap
+int nheap = 0;
+int heap[MAX_NODE] = { 0, };
+int weighted[MAX_NODE] = { 0, };
+int parent[MAX_NODE] = { 0, };
 
-//Stack
+
+//------------------------Stack-----------------------------
 int Push(int t)
 {
 	if (top >= MAX_NODE - 1)
@@ -60,15 +69,15 @@ int Stack_Empty()
 }
 
 
-//adjmatrix
+//-----------------------adjmatrix----------------------------
 int GM[MAX_NODE][MAX_NODE];
-FILE* fp;
+
 
 void input_adjmatrix(int a[][MAX_NODE], int* V, int* E)
 {
 	char vertex[3];
 	int i, j, k;
-	printf("\nInput number of node & edge\n");
+	printf("Input number of node & edge\n");
 	scanf("%d %d", V, E);
 
 	for (i = 0; i < *V; i++)
@@ -115,9 +124,10 @@ void print_adjmatrix(int a[][MAX_NODE], int V)
 }
 
 
-//adjlist
+//------------------------adjlist----------------------------
 typedef struct Node {
 	int vertex;
+	int weight;
 	struct Node* next;
 }Node;
 
@@ -130,11 +140,11 @@ void input_adjlist(Node* a[], int* V, int* E)
 
 	char vertex[3];	//입력받을 문자열 공간
 
-	int i, j;
+	int i, j, w;	//w= weight
 
 	//노드와 엣지의 개수입력받기
 	printf("\nInput number of node & edge\n");
-	scanf("%d %d", V, E);
+	fscanf(fp, "%d %d", V, E);
 
 	//노드 포인터 타입 배열 초기화 (포인터라 NULL로 초기화)
 	for (i = 0; i < *V; i++)
@@ -142,15 +152,17 @@ void input_adjlist(Node* a[], int* V, int* E)
 		a[i] = NULL;
 	}
 
+	printf("Input two node consist of edge & weight");
+
 	for (int j = 0; j < *E; j++)
 	{
-		printf("Input two node consist of edge ->");
-		scanf("%s", vertex);	//AB를 입력받고,AC를 입력받고
+		fscanf(fp,"%s %d", vertex, &w);	//AB를 입력받고,AC를 입력받고
 		i = name2int(vertex[0]);	//A-> ,A->
 		t = (Node*)malloc(sizeof(Node));	//노드생성
 		t->vertex = name2int(vertex[1]);	//B를 넣어주고, C를넣어주고
 		t->next = a[i];	//B->NULL를 해준다, C->B를해준다
 		a[i] = t;	//A->B, A->C
+		t->weight = w;	//input weight
 
 		//symetric(대칭)
 		i = name2int(vertex[1]);
@@ -158,10 +170,9 @@ void input_adjlist(Node* a[], int* V, int* E)
 		t->vertex = name2int(vertex[0]);
 		t->next = a[i];
 		a[i] = t;
-		printf("\n");
+		t->weight = w;
 	}
-
-	printf("\n");
+	printf("\n\n");
 }
 
 void print_adjlist(Node* a[], int V)
@@ -175,7 +186,7 @@ void print_adjlist(Node* a[], int V)
 		for (t = a[i]; t != NULL; t = t->next)
 		{
 			//t의 ->vertex를 출력한다.
-			printf("->%3c ", int2name(t->vertex));
+			printf(" -> %c:%d ", int2name(t->vertex), t->weight);
 		}
 		//줄바꿈
 		printf("\n");
@@ -183,6 +194,8 @@ void print_adjlist(Node* a[], int V)
 	printf("\n");
 }
 
+
+//------------------------DFS------------------------------
 //recursive version DFS
 void DFS_recur_list(Node* a[], int V, int i)
 {
@@ -246,7 +259,7 @@ void nrDFS_adjlist(Node* a[], int V)
 }
 
 
-//Find AP
+//----------------------Find AP----------------------------
 int AP_recur(Node* a[], int i)
 {
 	Node* t; 
@@ -301,27 +314,214 @@ void AP_search(Node* a[], int V)
 }
 
 
+//----------------------PFS--------------------------------
+Node* G[MAX_NODE];
+
+int pq_empty()
+{
+	if (nheap == 0)
+		return 1;
+	return 0;
+}
+
+void pq_init()
+{
+	nheap = 0;	//힙의 길이 = 0
+}
+
+void upheap(int h[], int k)
+{
+	int v; //index
+	v = h[k];
+
+	while (weighted[h[k / 2]] <= weighted[v] && k / 2 > 0)
+	{
+		h[k] = h[k / 2];
+		k /= 2;
+	}
+
+	h[k] = v;
+}
+
+void downheap(int h[], int k)
+{
+	int i, v;	//v=index
+	v = h[k];
+
+	while (k <= nheap / 2)
+	{
+		i = k << 1;
+
+		if (i < nheap && weighted[h[i]] < weighted[h[i + 1]])
+			i++;
+		if (weighted[v] >= weighted[h[i]])
+			break;
+		h[k] = h[i];
+		k = i;
+
+	}
+
+	h[k] = v;
+}
+
+//heap 정렬
+void adjust_heap(int h[], int n)
+{
+	int k;
+	for (k = n / 2; k >= 1; k--)
+		downheap(h, k);
+}
+
+//heap 추출
+int pq_extract(int h[])
+{
+	int v = h[1];	//v에 heap queue의 첫 index 추출
+
+	h[1] = h[nheap--];	//마지막항목을 첫 index로
+	downheap(h, 1);	//정렬
+
+	return v;
+}
+
+void print_heap(int h[])
+{
+	printf("\n");
+	for (int i = 1; i <= nheap; i++)
+		printf("  %c:%d ", int2name(h[i]), weighted[h[i]]);
+}
+
+int pq_update(int h[], int v, int p)	//v=index, p=weight
+{
+	if (weighted[v] == UNSEEN)	//UNSEEN Node
+	{
+		h[++nheap] = v;	//1부터, v=index
+		weighted[v] = p; //store the weight
+		upheap(h, nheap);	//정렬
+		return 1;	//업데이트 성공
+	}
+	else
+	{
+		if (weighted[v] < p)	//Fringe Node
+		{
+			weighted[v] = p;	//우선순위 변경
+			adjust_heap(h, nheap);	//힙 정렬
+			return 1;	//업데이트 성공
+		}
+		else
+			return 0;	//업데이트 할 거 없음
+	}
+}
+
+void PFS_adjlist(Node* g[], int V)
+{
+	int i;
+	Node* t;
+	pq_init();	//힙의 길이 = 0
+
+	//Init
+	for (i = 0; i < V; i++)
+	{
+		weighted[i] = UNSEEN; //가중치 = UNSEEN, 우선순위가 최하
+		parent[i] = 0; //부모노드
+	}
+
+	for (i = 0; i < V; i++)
+	{
+		if (weighted[i] == UNSEEN)	//방문 안했을때
+		{
+			parent[i] = -1;
+			pq_update(heap, i, UNSEEN); //힙 재조정(가중치)
+
+			while (!pq_empty())	//힙에 데이터가 존재할때
+			{
+				print_heap(heap);	//heap출력(index:weighted)
+				printf(" --> %c", int2name(heap[1]));
+				i = pq_extract(heap);	//추출 = 나무정점
+				weighted[i] = -weighted[i];	//양수로 바꿔줌
+
+				for (t = g[i]; t != NULL; t = t->next)
+				{
+					if (weighted[t->vertex] < 0) //UNSEEN or Fringe
+						//가중치가 업데이트 되면
+						if (pq_update(heap, t->vertex, -t->weight))
+							parent[t->vertex] = i;	//부모는 i이다
+				}
+			}
+		}
+	}
+
+	printf("\n\n");
+}
+
+//print parents and son
+void print_tree(int p[],int V)
+{
+	//node의 개수만큼 알파벳 순차출력
+	printf("Son:    ");
+	for (int i = 0; i < V; i++)
+		printf("%c ", int2name(i));	
+
+	//해당 노드의 부모 노드 출력(첫번째 노드는 .)
+	printf("\nParent: . ");
+	for (int i = 1; i < V; i++)
+		printf("%c ", int2name(p[i]));
+
+	printf("\n");
+}
+
+void print_cost(int w[],int V)
+{
+	int cost = 0;
+	//최소비용은 weight를 모두 더한 값
+	for (int i = 1; i < V; i++)
+	{
+		cost += w[i];
+	}
+	printf("\'%d'\n", cost);
+}
+
 int main()
 {
 	int V, E;
+	fp = fopen("graph.txt", "rt");
 
+	/*
 	//for adjacency matrix
 	//input_adjmatrix(GM, &V, &E);
 	//print_adjmatrix(GM, V);
 
 	//for adjacency list
-	input_adjlist(GL, &V, &E);
-	print_adjlist(GL, V);
+	//17 18 AB AC AD BE CF DH EF FH EG GI HI JK JL MN MO NP NQ OQ
+	//input_adjlist(GL, &V, &E);
+	//print_adjlist(GL, V);
 
 
 	//traversse the given graph
 	//DFS_adjlist(GL, V);
 	//nrDFS_adjlist(GL, V);
 
-	AP_search(GL, V);
+	//Add DJ JM
+	//AP_search(GL, V);
 	
-	//17 18 AB AC AD BE CF DH EF FH EG GI HI JK JL MN MO NP NQ OQ
-	//DJ JM
+	*/
+
+	//for PFS searching
+	//11 17 AB 4 AC 1 AD 2 AE 3 CD 2 DF 4 EF 4 BF 4
+	//DG 4 GH 3 HI 2 GI 3 GJ 4 IJ 2 JK 1 FJ 2 FK 4
+	input_adjlist(G, &V, &E);
+	
+	printf("\nOriginal graph\n");
+	print_adjlist(G, V);
+
+	printf("\nVisit order of Minimum Spanning Tree");
+	PFS_adjlist(G, V);
+	
+	print_tree(parent,V);
+	printf("\nMinimum Cost is ");
+	print_cost(weighted,V);
+
+	fclose(fp);
+
 
 	return 0;
 }
